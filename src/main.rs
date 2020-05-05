eacd3d2bb72d7d0e629d33b66f28b287b767ff45
naari3 <naari.named@gmail.com>
2020-05-05 17:51:22 +0900

    Copy and pasted tcp server example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::{Error, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handler(mut stream: TcpStream) -> Result<(), Error> {
+    println!("Connection from {}", stream.peer_addr()?);
+    let mut buffer = [0; 1024];
+    loop {
+        let nbytes = stream.read(&mut buffer)?;
+        if nbytes == 0 {
+            return Ok(());
+        }
+        println!("Message from {}: {:?}", stream.peer_addr()?, &buffer[..nbytes]);
+        stream.write(&buffer[..nbytes])?;
+        stream.flush()?;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:25565").expect("Error. failed to bind.");
+    for streams in listener.incoming() {
+        match streams {
+            Err(e) => eprintln!("error: {}", e),
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handler(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
+                });
+            }
+        }
+    }
 }
