a5852aac08b7f20a65558cf3f71bb1e46ed0cec8
naari3 <naari.named@gmail.com>
2021-11-27 00:49:43 +0900

    gif animation

--- src/slp.rs
+++ src/slp.rs
@@ -1,10 +1,61 @@
-use std::io::Error;
+use std::fs::File;
+use std::io::{Cursor, Result};
 
+use image::{
+    gif::GifDecoder, imageops::resize, png::PngEncoder, AnimationDecoder, EncodableLayout, Frame,
+};
+use once_cell::sync::Lazy;
 use serde::Serialize;
+use tokio::sync::Mutex;
 
 use crate::packet::client::{self, Pong, SlpResponse};
 use crate::packet::server::{Ping, Request};
 use crate::server::Worker;
+
+pub struct GifRunner {
+    frames: Vec<Frame>,
+    index: usize,
+}
+
+impl GifRunner {
+    fn new(gif_decoder: GifDecoder<File>) -> Self {
+        Self {
+            frames: gif_decoder.into_frames().collect_frames().unwrap(),
+            index: 0,
+        }
+    }
+
+    pub fn next(&mut self) -> Result<String> {
+        self.index = (self.index + 1) % self.frames.len();
+        let frame = self.frames.get(self.index).expect("should be exist");
+        let buffer = frame.buffer();
+        let resized = resize(buffer, 64, 64, image::imageops::FilterType::Gaussian);
+        let mut png_cursor = Cursor::new(vec![]);
+
+        let png_encoder = PngEncoder::new(&mut png_cursor);
+        png_encoder
+            .encode(
+                resized.as_bytes(),
+                resized.width(),
+                resized.height(),
+                image::ColorType::Rgba8,
+            )
+            .unwrap();
+
+        let data = base64::encode(png_cursor.into_inner());
+        let data = format!("data:image/png;base64,{}", data);
+
+        Ok(data)
+    }
+}
+
+static GIF: Lazy<Mutex<GifRunner>> = Lazy::new(|| {
+    let input = File::open("nc3132.gif").unwrap();
+    // Configure the decoder such that it will expand the image to RGBA.
+    let gif_decoder = GifDecoder::new(input).unwrap();
+    let gif = GifRunner::new(gif_decoder);
+    Mutex::new(gif)
+});
 
 #[derive(Debug, Serialize)]
 pub struct Description {
@@ -39,19 +90,19 @@ pub struct StatusResponse {
     pub version: Version,
 }
 
-pub async fn handle_slp_status(worker: &mut Worker, _request: Request) -> Result<(), Error> {
+pub async fn handle_slp_status(worker: &mut Worker, _request: Request) -> Result<()> {
     println!("get status request");
 
     let status_response = StatusResponse {
         description: Description {
-            text: "Yo this was implemented by naari3 @naari_ @_naari_".to_string(),
+            text: "this made by naari3 @naari_".to_string(),
         },
         players: Players {
             max: 12345,
             online: 126534640, // Japan population
             sample: Some(vec![]),
         },
-        favicon: None,
+        favicon: GIF.lock().await.next().ok(),
         version: Version {
             name: "1.15.2".to_string(),
             protocol: 578,
@@ -69,7 +120,7 @@ pub async fn handle_slp_status(worker: &mut Worker, _request: Request) -> Result
     Ok(())
 }
 
-pub async fn handle_slp_ping(worker: &mut Worker, ping: Ping) -> Result<(), Error> {
+pub async fn handle_slp_ping(worker: &mut Worker, ping: Ping) -> Result<()> {
     println!("get ping");
 
     let pong = client::StatusPacket::Pong(Pong {
